2e12bceb44ca04e678da119788589e7b7b1fb73d
Cesar Augusto Diaz Arriaga <cesar.a.diaz@gmail.com>
2021-08-14 00:31:39 -0500

    Commit Inicial

---
+++ src/main/java/co/edu/utp/gia/cdiaz/demomultitenancy/DemoMultitenancyApplication.java
@@ -0,0 +1,13 @@
+package co.edu.utp.gia.cdiaz.demomultitenancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMultitenancyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMultitenancyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/utp/gia/cdiaz/demomultitenancy/DemoMultitenancyApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.utp.gia.cdiaz.demomultitenancy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMultitenancyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
