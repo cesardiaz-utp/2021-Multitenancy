79b811dc0a32990bb2e53985139973b0dcec4148
Cesar Augusto Diaz Arriaga <cesar.a.diaz@gmail.com>
2021-08-14 01:01:36 -0500

    Capa Web

---
+++ src/main/java/co/edu/utp/gia/cdiaz/demomultitenancy/web/CarroController.java
@@ -0,0 +1,38 @@
+package co.edu.utp.gia.cdiaz.demomultitenancy.web;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.utp.gia.cdiaz.demomultitenancy.modelo.entidades.Carro;
+import co.edu.utp.gia.cdiaz.demomultitenancy.modelo.repositorios.CarroRepository;
+
+@RestController
+@RequestMapping("/cars")
+public class CarroController {
+
+    private CarroRepository repository;
+
+    public CarroController(CarroRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Carro insert(@RequestBody Carro car) {
+        return repository.save(car);
+    }
+
+    @GetMapping
+    public List<Carro> posts() {
+        return repository.findAll();
+    }
+
+}
